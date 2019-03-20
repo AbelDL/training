9512ac91a8212737ef1d17d8f1a5568bfdd8d23b
Abel <luca971001@gmail.com>
2019-03-20 13:30:41 -0600

    primera practica de javascript

--- content/1-Values/practice.js
+++ content/1-Values/practice.js
@@ -1,7 +1,64 @@
 // Best representation in Javascript for:
 // - The tax percentage for purchases in Mexico
+const taxpercent = 16
 // - A terms && conditions document
 // - Houses in Game of thrones
+const houseName = 'cazadorez';
+const houseName2 = 'agricultores';
+const houseName3 = 'soldados';
+
+const houses = [
+    'soldados',
+    'agricultores',
+    'cazadores'
+];
+
 // - The simplest to-do list you can think of
+const list = [
+    'Al depertar tender la cama'
+];
+let lista2 =
+    ['lavar la ropa',
+        'lavar los trastes',
+        'estudiar'
+    ];
+
+console.log({ taxpercent, houses, list, lista2 })
+
 // - Teams in the world cup
+let teamscup = ['16']
+
+let team1 = 'Brasil';
+let team2 = 'Mexico';
+let team3 = 'USA';
+let team4 = 'Jamaica';
+let team5 = 'Korea';
+let team6 = 'Holanda';
+let team7 = 'Francia';
+let team8 = 'Alemania';
+let team9 = 'Rusia';
+let team10 = 'Costa de marfil';
+let team11 = 'Africa';
+let team12 = 'China';
+let team13 = 'Argentina';
+let team14 = 'Chile';
+let team15 = 'Peru'
+let team16 = 'Panama';
+
+console.log(team1, team10, team11)
+
 // - The list of students in the class with name, age and favorite programming language.
+const students = [
+    { name: 'Mario', age: 20, programlanguage: 'c++' },
+    { name: 'Carlos', age: 19, programlanguage: 'piton' },
+    { name: 'Saul', age: 18, programlanguage: 'java' },
+    { name: 'Pedro', age: 18, programlanguage: 'java' },
+    { name: 'Hugo', age: 21, programlanguage: 'c++' }
+];
+
+const pikqchu = {
+    name: 'pikachu',
+    height: 20
+}
+
+console.log(pikqchu)
