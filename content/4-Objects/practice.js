8187bec49926d07f69061ad35d7d3fa1b1aeca4d
Abel <luca971001@gmail.com>
2019-03-21 14:04:12 -0600

    Practica 4 de javascript

--- content/4-Objects/practice.js
+++ content/4-Objects/practice.js
@@ -5,10 +5,30 @@
 
 const products = [
   { name: 'Kindle', price: 199 },
-  { name: 'iPad', price: 299 },
+  { name: 'iPad', price: 299 },
   { name: 'Fire', price: 89 },
 ];
 
-const Cart = {
-  
-}
+const cart = {
+  name: 'shoppingCart',
+  size: 'medium',
+  allowedproducts: 10,
+  color: 'red',
+  products: [
+    { name: 'laptop', price: 4500 },
+    { name: 'dvd', price: 800 },
+    { name: 'mini stereo', price: 3800 },
+    { name: 'smartphone', price: 2400 },
+    { name: 'phone charger', price: 100 },
+    { name: 'USB fan', price: 80 },
+  ],
+
+  total: function () {
+    return this.products.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }
+};
+
+console.log (cart.total());
+
