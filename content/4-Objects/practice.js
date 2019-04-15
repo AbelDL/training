e8799b09931bb0fb83aad4acdbe35ec63e969f6b
Abel <luca971001@gmail.com>
2019-04-15 15:45:29 -0500

    practica 4 complementada

--- content/4-Objects/practice.js
+++ content/4-Objects/practice.js
@@ -10,31 +10,34 @@ const products = [
 ];
 
 const cart = {
-  name: 'shoppingCart',
-  size: 'medium',
+  name: 'ShoppingCart',
+  size: 'Medium',
   allowedproducts: 10,
-  color: 'red',
+  color: 'Red',
   products: [
-    { name: 'laptop', price: 4500 },
-    { name: 'dvd', price: 800 },
-    { name: 'mini stereo', price: 3800 },
-    { name: 'smartphone', price: 2400 },
-    { name: 'phone charger', price: 100 },
-    { name: 'USB fan', price: 80 },
+    { name: 'LAPTOP', price: 7500, quantity: '3' },
+    { name: 'DVD', price: 800, quantity: '1' },
+    { name: 'HOME THEATER', price: 4800, quantity: '2' },
+    { name: 'SMARTPHONE', price: 10400, quantity: '4' },
+    { name: 'PHONE CHARGER', price: 100, quantity: '2' },
+    { name: 'USB fAN', price: 80, quantity: '10' },
   ],
 
   total: function () {
     return this.products.reduce((total, product) => {
-      return total + product.price;
+      return total + product.price * product.quantity;
     }, 0);
-  },
-
-  list: function () {
-    for (const p of this.products) {
-      console.log(`${p.name} - ${p.price}`);
-    }
   }
 };
+console.log(`\nLos productos de carrito son:\n`, cart.products, `      \n          `)
+const precioAltoBajo = cart.products.sort((a, b) => {
+  return a.quantity - b.quantity;
+});
 
-console.log(cart.total());
-cart.list();
+const listaDeProductos = cart.products.forEach((cart, index) => {
+  console.log(`${index + 1}-${cart.name}-${cart.price}`)
+});
+
+console.log(`\nEl total de su compra es de: ${cart.total()}`);
+
+
