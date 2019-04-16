f9fd1e7ad765f6c4ffdc36c331f80e9932d80b87
AbelDL <luca971001@gmail.com>
2019-04-15 19:57:33 -0500

    agregue otro metodo de implementacion

--- content/4-Objects/practice.js
+++ content/4-Objects/practice.js
@@ -45,41 +45,39 @@ cart.list();
 //    - It can list all the products inside it in the format "name" - "price"
 //    - It can sum the total
 
-// const products = [
-//   { name: 'Kindle', price: 199 },
-//   { name: 'iPad', price: 299 },
-//   { name: 'Fire', price: 89 },
-// ];
 
-// const cart = {
-//   name: 'ShoppingCart',
-//   size: 'Medium',
-//   allowedproducts: 10,
-//   color: 'Red',
-//   products: [
-//     { name: 'LAPTOP', price: 7500, quantity: '3' },
-//     { name: 'DVD', price: 800, quantity: '1' },
-//     { name: 'HOME THEATER', price: 4800, quantity: '2' },
-//     { name: 'SMARTPHONE', price: 10400, quantity: '4' },
-//     { name: 'PHONE CHARGER', price: 100, quantity: '2' },
-//     { name: 'USB fAN', price: 80, quantity: '10' },
-//   ],
+const products = [
+  { name: 'Kindle', price: 199 },
+  { name: 'iPad', price: 299 },
+  { name: 'Fire', price: 89 },
+];
 
-//   total: function () {
-//     return this.products.reduce((total, product) => {
-//       return total + product.price * product.quantity;
-//     }, 0);
-//   }
-// };
-// const precioAltoBajo = cart.products.sort((a, b) => {
-//   return a.quantity - b.quantity;
-// });
+const cart = {
+  name: 'ShoppingCart',
+  size: 'Medium',
+  allowedproducts: 10,
+  color: 'Red',
+  products: [
+    { name: 'LAPTOP', price: 7500, quantity: '3' },
+    { name: 'DVD', price: 800, quantity: '1' },
+    { name: 'HOME THEATER', price: 4800, quantity: '2' },
+    { name: 'SMARTPHONE', price: 10400, quantity: '4' },
+    { name: 'PHONE CHARGER', price: 100, quantity: '2' },
+    { name: 'USB fAN', price: 80, quantity: '10' },
+  ],
 
-// const listaDeProductos = cart.products.forEach((cart, index) => {
-//   console.log(`${index + 1}-${cart.name} - $${cart.price}\n Cantidad:${cart.quantity}`)
-// });
+  total: function () {
+    return this.products.reduce((total, product) => {
+      return total + product.price * product.quantity;
+    }, 0);
+  }
+};
+const precioAltoBajo = cart.products.sort((a, b) => {
+  return a.quantity - b.quantity;
+});
 
+const listaDeProductos = cart.products.forEach((cart, index) => {
+  console.log(`${index + 1}-${cart.name} - $${cart.price}\n Cantidad:${cart.quantity}`)
+});
 
-// console.log(`\n "TICKET" \n El total de su compra es de: ${cart.total()}`);
-
-
+console.log(`\n "TICKET" \n El total de su compra es de: ${cart.total()}`);
